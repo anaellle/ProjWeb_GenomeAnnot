96c95f480de1313ebc27e9480f9f64d39d2b2888
Elora-V <elora95.vigo@gmail.com>
2024-01-05 10:39:31 +0100

    test view tab active

--- GenomeAnnot/blast/tests.py
+++ GenomeAnnot/blast/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class ExploreViewTest(TestCase):
+    def test_main_blast_view(self):
+        # Create a client
+        client = Client()
+
+        # get request to the blast view
+        url = reverse("blast:index")
+        response = client.get(url)
+
+        # Check 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check template
+        self.assertTemplateUsed(response, "blast/main_blast.html")
+
+        # Check the value of active_tab
+        self.assertEqual(response.context["active_tab"], "blast")
--- GenomeAnnot/validate/tests.py
+++ GenomeAnnot/validate/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class ExploreViewTest(TestCase):
+    def test_main_validate_view(self):
+        # Create a client
+        client = Client()
+
+        # get request to the validate view
+        url = reverse("validate:index")
+        response = client.get(url)
+
+        # Check 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check template
+        self.assertTemplateUsed(response, "validate/main_validate.html")
+
+        # Check the value of active_tab
+        self.assertEqual(response.context["active_tab"], "validate")
