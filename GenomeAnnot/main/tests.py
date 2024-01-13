d03a81fb0f21454f548baad3615ac22752b0b59f
Elora-V <elora95.vigo@gmail.com>
2024-01-13 14:07:04 +0100

    vue base addgenome, admin, home

--- GenomeAnnot/main/tests.py
+++ GenomeAnnot/main/tests.py
@@ -15,7 +15,7 @@ class ViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # Check template
-        self.assertTemplateUsed(response, "main/main_annotate.html")
+        self.assertTemplateUsed(response, "main/annotate/main_annotate.html")
 
         # Check the value of active_tab
         self.assertEqual(response.context["active_tab"], "annotate")
@@ -32,7 +32,7 @@ class ViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # Check template
-        self.assertTemplateUsed(response, "main/main_blast.html")
+        self.assertTemplateUsed(response, "main/blast/main_blast.html")
 
         # Check the value of active_tab
         self.assertEqual(response.context["active_tab"], "blast")
@@ -49,7 +49,7 @@ class ViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # Check template
-        self.assertTemplateUsed(response, "main/main_explore.html")
+        self.assertTemplateUsed(response, "main/explore/main_explore.html")
 
         # Check the value of active_tab
         self.assertEqual(response.context["active_tab"], "explore")
@@ -66,7 +66,95 @@ class ViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # Check template
-        self.assertTemplateUsed(response, "main/main_validate.html")
+        self.assertTemplateUsed(response, "main/validate/main_validate.html")
 
         # Check the value of active_tab
         self.assertEqual(response.context["active_tab"], "validate")
+
+    def test_home_view(self):
+        # Create a client
+        client = Client()
+
+        # get request to the validate view
+        url = reverse("main:home")
+        response = client.get(url)
+
+        # Check 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check template
+        self.assertTemplateUsed(response, "main/home.html")
+
+        # Check the value of active_tab
+        self.assertEqual(response.context["active_tab"], "home")
+
+    def test_addGenome_view(self):
+        # Create a client
+        client = Client()
+
+        # get request to the validate view
+        url = reverse("main:addGenome")
+        response = client.get(url)
+
+        # Check 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check template
+        self.assertTemplateUsed(response, "main/addGenome/addGenome.html")
+
+        # Check the value of active_tab
+        self.assertNotIn("active_tab", response.context)
+
+    def test_genomeAdmin_view(self):
+        # Create a client
+        client = Client()
+
+        # get request to the validate view
+        url = reverse("main:genomeAdmin")
+        response = client.get(url)
+
+        # Check 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check template
+        self.assertTemplateUsed(response, "main/admin/admin_genome.html")
+
+        # Check the value of active_tab
+        self.assertEqual(response.context["active_tab"], "admin")
+        self.assertEqual(response.context["active_tab_admin"], "genome")
+
+    def test_sequenceAdmin_view(self):
+        # Create a client
+        client = Client()
+
+        # get request to the validate view
+        url = reverse("main:sequenceAdmin")
+        response = client.get(url)
+
+        # Check 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check template
+        self.assertTemplateUsed(response, "main/admin/admin_sequence.html")
+
+        # Check the value of active_tab
+        self.assertEqual(response.context["active_tab"], "admin")
+        self.assertEqual(response.context["active_tab_admin"], "sequence")
+
+    def test_accountAdmin_view(self):
+        # Create a client
+        client = Client()
+
+        # get request to the validate view
+        url = reverse("main:accountAdmin")
+        response = client.get(url)
+
+        # Check 200 status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check template
+        self.assertTemplateUsed(response, "main/admin/admin_account.html")
+
+        # Check the value of active_tab
+        self.assertEqual(response.context["active_tab"], "admin")
+        self.assertEqual(response.context["active_tab_admin"], "account")
--- GenomeAnnot/main/urls.py
+++ GenomeAnnot/main/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 app_name = "main"
 urlpatterns = [
+    path("", views.home, name="home"),
     path("explore/", views.explore, name="explore"),
     path("annotate/", views.annotate, name="annotate"),
     path("validate/", views.validate, name="validate"),
     path("blast/", views.blast, name="blast"),
+    path("admin/genome/", views.genomeAdmin, name="genomeAdmin"),
+    path("admin/sequence/", views.sequenceAdmin, name="sequenceAdmin"),
+    path("admin/account/", views.accountAdmin, name="accountAdmin"),
+    path("addGenome", views.addGenome, name="addGenome"),
 ]
