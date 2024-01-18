3baf0cc0e5ae6b2f47f1c5aecc097750833f4ac8
Elora-V <elora95.vigo@gmail.com>
2024-01-18 14:11:20 +0100

    scroll up

--- GenomeAnnot/main/static/js_script.js
+++ GenomeAnnot/main/static/js_script.js
@@ -1,7 +1,29 @@
 
+// --------------- Scroll button
 
-// Change filter of explore depending on the selection before search bar
-const typeSearch = document.getElementById('res_type-select');
+$(document).ready(function() {
+    var sizeFooter = $('footer').height();
+    var sizeButton=$('#scrollTop').height();
+    $('#scrollTop').css("bottom", (sizeButton+sizeFooter+10)+"px");
+});
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  } 
+
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    // When the user scrolls down 20px from the top of the document, show the button
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $("#scrollTop").show();
+  } else {
+    $("#scrollTop").hide();
+  }
+}
+// ---------------- Change filter of explore depending on the selection before search bar
 
 function changeTypeResExplore(){
     const selectedValue = $("#res_type-select")[0].value;
