f62943da724033aad8bb8ad7cfdf363841209e40
Elora-V <elora95.vigo@gmail.com>
2024-01-23 15:57:43 +0100

    debut filtre form

--- GenomeAnnot/main/views.py
+++ GenomeAnnot/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+# Library required for lauching the Blast API
+from Bio.Blast import NCBIWWW
+from Bio import SeqIO
+from Bio import SearchIO
+
 
 def home(request):
     context = {"active_tab": "home"}
@@ -7,7 +12,49 @@ def home(request):
 
 
 def explore(request):
-    context = {"active_tab": "explore"}
+    context = {
+        "active_tab": "explore",
+    }
+    if request.method == "GET":
+        if "submitsearch" in request.GET:
+            # get parameters of search
+            searchbar = request.GET.get("searchbar")
+            type_res = request.GET.get("res_type")
+            context["searchbar"] = searchbar
+            context["type_res"] = type_res
+
+            if type_res == "genome":
+                strain = request.GET.get("strain")
+                species = request.GET.get("species")
+                """ status_genome_0 = request.GET.get("0")
+                status_genome_1 = request.GET.get("1")
+                status_genome_2 = request.GET.get("2") """
+                context["strain"] = strain
+                context["species"] = species
+                """ if "0" in request.GET:
+                    context["test"] = "check"
+                else:
+                    context["test"] = "uncheck"
+                context["status_genome_0"] = status_genome_0
+                context["status_genome_1"] = status_genome_1
+                context["status_genome_2"] = status_genome_2 """
+
+            elif type_res == "gene" or type_res == "prot":
+                genome = request.GET.get("genome")
+                chrom = request.GET.get("chrom")
+                motif = request.GET.get("motif")
+                seq = request.GET.get("seq")
+                """status_gene_0 = request.GET.get("status0")
+                status_gene_123 = request.GET.get("status1")
+                status_gene_4 = request.GET.get("status4") """
+                context["genome"] = genome
+                context["chrom"] = chrom
+                context["motif"] = motif
+                context["seq"] = seq
+                """ context["status_gene_0"] = status_gene_0
+                context["status_gene_123"] = status_gene_123
+                context["status_gene_4"] = status_gene_4 """
+
     return render(request, "main/explore/main_explore.html", context)
 
 
@@ -20,32 +67,40 @@ def validate(request):
     context = {"active_tab": "validate"}
     return render(request, "main/validate/main_validate.html", context)
 
-#Library required for lauching the Blast API  
-from Bio.Blast import NCBIWWW
-from Bio import SeqIO
-from Bio import SearchIO
 
 def blast(request):
     context = {"active_tab": "blast"}
-    #return render(request, "main/blast/main_blast.html", context)
+    # return render(request, "main/blast/main_blast.html", context)
 
-    if request.method == 'POST':
-        sequence = request.POST['sequence']
-        parameters = request.POST['parameters']
+    if request.method == "POST":
+        sequence = request.POST["sequence"]
+        parameters = request.POST["parameters"]
 
-        #Request to ncbi blast api, rajouter gestion des erreurs ensuite
-        #try:
-        result_handle = NCBIWWW.qblast(program="blastn", database="nt", sequence=sequence, alignments=5, descriptions=5) #,format_type="Text") #Parametres de base pour le moment, rajouter un choix apres
-        blast_results = SearchIO.read(result_handle, "blast-xml") #permet recuperation dans le template pour l'affichage
+        # Request to ncbi blast api, rajouter gestion des erreurs ensuite
+        # try:
+        result_handle = NCBIWWW.qblast(
+            program="blastn",
+            database="nt",
+            sequence=sequence,
+            alignments=5,
+            descriptions=5,
+        )  # ,format_type="Text") #Parametres de base pour le moment, rajouter un choix apres
+        blast_results = SearchIO.read(
+            result_handle, "blast-xml"
+        )  # permet recuperation dans le template pour l'affichage
 
-        #except Exception as e:
-            # Gérer les erreurs, par exemple, en renvoyant un message d'erreur à l'utilisateur
-            #return render(request, 'error.html', {'error_message': str(e)})
+        # except Exception as e:
+        # Gérer les erreurs, par exemple, en renvoyant un message d'erreur à l'utilisateur
+        # return render(request, 'error.html', {'error_message': str(e)})
 
         # Traiter les résultats et afficher dans le template
-        return render(request, 'main/blast/blast_results.html', {"active_tab": "blast",'results': blast_results})
+        return render(
+            request,
+            "main/blast/blast_results.html",
+            {"active_tab": "blast", "results": blast_results},
+        )
 
-    return render(request, "main/blast/main_blast.html",context)
+    return render(request, "main/blast/main_blast.html", context)
 
 
 def genomeAdmin(request):
