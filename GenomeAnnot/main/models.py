da1179773b6ef40003da6d81be50df29bf37cdd9
Emeline Bruyère <emlinebruyere@gmail.com>
2024-01-28 14:38:57 +0100

    ajout de l'attribut description dans peptide

--- GenomeAnnot/main/models.py
+++ GenomeAnnot/main/models.py
@@ -94,7 +94,7 @@ class Gene (models.Model):
     strand = models.IntegerField(choices=Strand.choices, default=Strand.SENSE)
     startPos = models.IntegerField()
     endPos = models.IntegerField()
-    description = models.CharField(max_length=700, blank=True)
+    description = models.CharField(max_length=1000, blank=True)
     status = models.IntegerField(choices=Status.choices, default=Status.ASSIGNABLE)
     
     idChrom = models.ForeignKey(Chromosome, on_delete=models.CASCADE)
@@ -131,6 +131,7 @@ class Peptide(models.Model):
     id = models.CharField(max_length=200, primary_key=True)
     transcriptName = models.CharField(max_length=200)
     transcriptBiotype = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=1000, blank=True)
     
     idGene = models.ForeignKey(Gene, on_delete=models.CASCADE)
     
