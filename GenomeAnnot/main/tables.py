8d16c93bdbf2ac938c0316e98f7f79b6ffccc7ee
Emeline Bruyère <emlinebruyere@gmail.com>
2024-02-14 01:41:53 +0100

    Annotate --> Annotated , and Validated transformed, for readers only, into Annotated

--- GenomeAnnot/main/tables.py
+++ GenomeAnnot/main/tables.py
@@ -79,7 +79,7 @@ class TableGenome(tables.Table):
                   fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">\
                 <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/> \
               </svg>\
-               Not Annotate  </span>'
+               Not Annotated  </span>'
         elif value == "In work":
             string = '  <span class="orange"> \
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" \
