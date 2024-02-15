ef073d131f9a43548e54b9b5b70c7f5322038f78
anacossard <anaelle.cossard@gmail.com>
2024-02-15 15:25:13 +0100

    Added documentation and help for uploading files

--- GenomeAnnot/main/management/commands/init-my-data.py
+++ GenomeAnnot/main/management/commands/init-my-data.py
@@ -4,8 +4,12 @@ import os
 
 class Command(BaseCommand):
     help = 'Initial filling of the database with files in the Data repository'
+    
     def handle(self, *args, **options):
-        # Your function goes here
+        '''
+        Fill the database with the fasta files available in the 
+        directory GenomeAnnot/Data
+        '''
         current_dir = os.getcwd()
         data_dir = os.path.join(current_dir, 'Data')
 
@@ -13,9 +17,11 @@ class Command(BaseCommand):
             # Get the list of files in the directory
             files_in_directory = os.listdir(data_dir)
             
+            # loop through files to get the genome, gene and peptide files together
             for file in files_in_directory:
                 name = file.split('.fa')[0]
                 
+                # get the name of the other files
                 if name.endswith('_cds'):
                     genomeName = name.split('_cds')[0]+'.fa'
                     geneName = name+'.fa'
@@ -31,10 +37,12 @@ class Command(BaseCommand):
                     geneName = name+'_cds.fa'
                     peptideName = name+'_pep.fa'
                 
+                # get the paths of the files
                 genomePath = os.path.join(data_dir,genomeName)
                 genePath = os.path.join(data_dir,geneName)
                 peptidePath = os.path.join(data_dir,peptideName)
                 
+                # check if they exists and add them to the database
                 if os.path.exists(genomePath) and os.path.exists(genePath) and os.path.exists(peptidePath):
                     self.stdout.write(self.style.WARNING('Adding {} genome'.format(genomeName)))
                     uploadAndFill(genomePath,genePath,peptidePath)
